c1864396db03a2c3f11e6bb8a760cf8604110089
lsryl13578 <tkdfuf1008@gmail.com>
2026-07-23 15:00:01 +0900

    fix: 인바디 ErrorCode 번호 중복 해결

--- src/main/java/com/example/MVP_MealFit/global/exception/ErrorCode.java
+++ src/main/java/com/example/MVP_MealFit/global/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
     OCR_PARSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "OCR_002", "OCR 응답을 해석할 수 없습니다."),
 
     INBODY_PARSE_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "INBODY_001", "인바디 결과에서 필요한 데이터를 추출할 수 없습니다."),
-    INBODY_NOT_FOUND(HttpStatus.NOT_FOUND, "INBODY_001", "인바디 정보를 찾을 수 없습니다."),
+    INBODY_NOT_FOUND(HttpStatus.NOT_FOUND, "INBODY_002", "인바디 정보를 찾을 수 없습니다."),
 
     FILE_INVALID_TYPE(HttpStatus.BAD_REQUEST, "FILE_001", "허용되지 않는 파일 형식입니다."),
     FILE_TOO_LARGE(HttpStatus.PAYLOAD_TOO_LARGE, "FILE_002", "파일 용량이 너무 큽니다.");
